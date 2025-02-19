5226a7cf97c53a89422ef5512e2d30e7ee234604
Bresman-vargas <163463362+Bresman-vargas@users.noreply.github.com>
2025-02-19 15:04:26 -0300

    AcmeLogo problem

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+// import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Page() {
   return (
@@ -18,7 +19,7 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
@@ -26,6 +27,8 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image src="/hero-desktop.png" width="1000" height="720" alt='Screenshot app' className='hidden lg:block'/>
+          <Image src="/hero-mobile.png" width="560" height="620" alt='Screenshot app' className='block lg:hidden'/>
         </div>
       </div>
     </main>
